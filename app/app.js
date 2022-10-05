f48559e0690458f8382e315f3298c74f5777024e
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-10-05 18:16:15 -0500

    add special attack and heal functionality

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         };
     },
     computed: {
@@ -15,17 +16,43 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return { width: this.playerHealth + '%' }
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
+
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+
+            this.attackPlayer();
         }
     }
 });
