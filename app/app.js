f4a581eaa85979ecba2c70f79d44d2181bbdaa9d
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-10-05 17:12:57 -0500

    add monster health & player health and  attacks

--- app/app.js
+++ app/app.js
@@ -1,11 +1,24 @@
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
     data() {
         return {
-
-        }
+            playerHealth: 100,
+            monsterHealth: 120
+        };
     },
     methods: {
-
+        attackMonster() {
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        }
     }
 });
 
