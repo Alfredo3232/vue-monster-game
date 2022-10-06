23d105753174146ba3e51175283a82ba77479b3f
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-10-06 13:24:51 -0500

    add server functionality and instructions to run it

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Vue_Monster_Project
+
+A Small Game where you as the player try to bring the monsters health to 0
+
+To see this simple application install the dependencies with `npm install`\
+Then run it with `npm start`\
+Then go to your browser at <http://localhost:3000>
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(__dirname + "/app"));
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/app/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log("Server is now running at PORT:", PORT);
+})
